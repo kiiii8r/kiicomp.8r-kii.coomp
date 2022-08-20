00717f7c86ec49492c7c6d0d66df97b7fe3c8f61
kiiii8r <mg-e.s90@outlook.jp>
2022-08-20 19:06:23 +0900

    change

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,24 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'World';
-  changeHandler(event) {
-    this.greeting = event.target.value;
+  name // 未定義
+  age=30;
+  fullname = "test san"
+  showData = false
+  details={
+    name:"dummy",
+    place:"Japan"
+  }
+  userList = ["A","B","C"]
+  title = "aura"
+
+  // 双方向バインディング
+  changeHandler(event){
+    this.title = event.target.value
+  }
+
+  address={
+    city: "kanagawa",
+    postcode: 0000000,
+    country: "Japan"
   }
 }
