6b581f36ad42d5bae4706dfc9e843d8a2e467f80
kiiii8r <mg-e.s90@outlook.jp>
2022-08-24 14:13:14 +0900

    add helloCondition

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -2,7 +2,18 @@ import { LightningElement } from 'lwc';
 
 export default class HelloConditionalRendering extends LightningElement {
     isVisible = false
+
+
+    name
     handleClick(){
         this.isVisible = true
+    }
+
+    changeHandler(event){
+        this.name = event.target.value
+    }
+
+    get helloMethod(){
+        return this.name === 'hello'
     }
 }
